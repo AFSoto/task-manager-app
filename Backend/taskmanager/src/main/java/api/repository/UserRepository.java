606aaaf03a44cd9b21c95a76f37313f16af2b6db
Andres Felipe Soto Quintero <andresfelipesotoquintero@gmail.com>
2026-03-09 00:15:36 -0500

    mejore execpciones de uusario y de priorty

--- Backend/taskmanager/src/main/java/api/repository/UserRepository.java
+++ Backend/taskmanager/src/main/java/api/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package api.repository;
+
 import api.model.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-public interface UserRepository  extends JpaRepository<User, Long>{
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    // optional es una clase contenedora que puede tener un valor o estar vacio esto
+    // evita errores
     Optional<User> findByEmail(String email);
-    
-} 
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByDocument(String document);
+}
